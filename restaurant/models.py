58ddfe37a7e5c0241c9d91daef1203ea1cf6fe99
alancoomeynts-hub <alancoomeynts@gmail.com>
2026-06-07 20:12:12 +0100

    Add restaurant model accidentally removed when fixing gitignore files which were committed

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField(blank=True)
+    email = models.EmailField(blank=True)
+    phone = models.CharField(max_length=20, blank=True)
+    address = models.TextField(blank=True)
+    open_at = models.TimeField(blank=True, null=True)
+    closes_at = models.TimeField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.name}'
