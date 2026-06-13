317ee8bc3ef658aa02df92192fad2f72978e2712
alancoomeynts-hub <alancoomeynts@gmail.com>
2026-06-13 12:48:04 +0100

    Add 404 template, update restaurant view and css styles

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Restaurant
 
 
 def restaurant_homepage(request):
-    restaurant = Restaurant.objects.first()
+    restaurant=get_object_or_404(Restaurant,pk=1)
     return render(request, 'restaurant/homepage.html', {'restaurant': restaurant})
