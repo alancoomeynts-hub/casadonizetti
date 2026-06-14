11cfb5f614ea7d5083c8689d9a9f79e52ca1e34a
alancoomeynts-hub <alancoomeynts@gmail.com>
2026-06-14 19:51:58 +0100

    fix footer context processor

--- restaurant/context_processors.py
+++ restaurant/context_processors.py
@@ -1,10 +1,10 @@
 from .models import Restaurant, SocialLink
 
 def footer(request):
-    restaurants = Restaurant.objects.first()
+    restaurant = Restaurant.objects.first()
     social_links = SocialLink.objects.all()
 
     return {
-        'restaurants': restaurants,
+        'restaurant': restaurant,
         'social_links': social_links
     }
