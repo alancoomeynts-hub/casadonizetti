cc20c08d41aaf4bd9a7686c34f582a497f224acc
alancoomeynts-hub <alancoomeynts@gmail.com>
2026-06-17 22:08:43 +0100

    Add admin classes for Table and Reservation

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Table, Reservation
 
 # Register your models here.
+
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('name','capacity','is_available',)
+    list_filter = ('is_available','capacity',)
+    search_fields = ('name',)
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('user','table','contact_name','contact_email','party_size','reservation_for')
+    list_filter = ('reservation_for','table','user')
+    search_fields = ('user__username','contact_name','contact_email')
