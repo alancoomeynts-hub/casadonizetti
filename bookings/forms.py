94a97799bea5409c42a63f1fc5b641f09b47a32e
alancoomeynts-hub <alancoomeynts@gmail.com>
2026-06-19 11:11:31 +0100

    Add Datetime widget for reservation form

--- bookings/forms.py
+++ bookings/forms.py
@@ -5,3 +5,9 @@ class ReservationForm(forms.ModelForm):
     class Meta:
         model = Reservation
         fields = ('contact_name', 'contact_email', 'contact_phone', 'party_size', 'reservation_for',)
+        widgets= {
+            'reservation_for': forms.DateTimeInput(format='%Y-%m-%dT%H:%M',attrs={
+                'type':'datetime-local',
+                'step':'900',
+                'class':'form-control' }),
+        }
