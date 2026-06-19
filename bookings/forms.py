df5e50faa784a7ca7bfdfd4f541fb7648a331488
alancoomeynts-hub <alancoomeynts@gmail.com>
2026-06-19 12:44:49 +0100

    Reserve form restructured. Add custom fields for date and time of reservation.

--- bookings/forms.py
+++ bookings/forms.py
@@ -2,12 +2,25 @@ from django import forms
 from .models import Reservation
 
 class ReservationForm(forms.ModelForm):
+
+    reservation_date=forms.DateField(widget=forms.DateInput(attrs={'type':'date'}))
+
+    TIME_CHOICES=[]
+
+    for i in range(12, 23, 1):
+        for j in range(0, 60, 15):
+            TIME_CHOICES.append((f"{i}:{j:02d}", f"{i}:{j:02d}"))
+
+    reservation_time=forms.ChoiceField(choices=TIME_CHOICES,
+                                       widget=forms.Select(attrs={'class':'form-select'}))
+
     class Meta:
         model = Reservation
-        fields = ('contact_name', 'contact_email', 'contact_phone', 'party_size', 'reservation_for',)
-        widgets= {
-            'reservation_for': forms.DateTimeInput(format='%Y-%m-%dT%H:%M',attrs={
-                'type':'datetime-local',
-                'step':'900',
-                'class':'form-control' }),
-        }
+        fields = (
+            'contact_name',
+            'contact_email',
+            'contact_phone',
+            'party_size',
+            'reservation_date',
+            'reservation_time',
+        )
