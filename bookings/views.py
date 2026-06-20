d0f9728adf3cd53260af0744fc5010349933d07a
alancoomeynts-hub <alancoomeynts@gmail.com>
2026-06-20 23:10:02 +0100

    Add reservation form post handling with date time conversion of reservation date and reservation time into reservation_for, and redirect to homepage.

--- bookings/views.py
+++ bookings/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from datetime import datetime
 from bookings.forms import ReservationForm
 
 # Create your views here.
 def reservation_view(request):
     reservation_form=ReservationForm(request.POST or None)
+    if request.method == 'POST':
+        form = ReservationForm(data=request.POST)
+        if form.is_valid():
+            reservation_form = form.save(commit=False)
+            date=form.cleaned_data['reservation_date']
+            time=form.cleaned_data['reservation_time']
+            time_converted=datetime.strptime(time,'%H:%M').time()
+            reservation_form.reservation_for=datetime.combine(date,time_converted)
+            reservation_form.save()
+
+            return redirect('homepage')
 
     return render(
         request,
