0c2257a4b7e8be2970fd85b51133482925b1d6a7
alancoomeynts-hub <alancoomeynts@gmail.com>
2026-06-30 19:20:42 +0100

    Update JS listeners with extra guard clauses.

--- static/js/main.js
+++ static/js/main.js
@@ -1,36 +1,47 @@
 document.addEventListener( "DOMContentLoaded", function() {
-
-    if (document.getElementById("profile-details")) {
+    const confirm_profile_page = document.getElementById("profile-details");
+    if (confirm_profile_page) {
         const deleteModal = document.getElementById('cancelReservationModal');
         const cancelConfirm = document.getElementById("cancelConfirm");
         const reservationText = document.getElementById("reservationModalText");
-        deleteModal.addEventListener('show.bs.modal', function (event) {
-            const button = event.relatedTarget;
-            const reservationDate = button.getAttribute('data-reservation-date');
-            const reservationId = button.getAttribute('data-reservation-id');
-            const cancelUrl = button.getAttribute('data-cancel-url');
-            reservationText.textContent = reservationDate;
-            cancelConfirm.action = cancelUrl
+
+        if(deleteModal && cancelConfirm && reservationText) {
+            deleteModal.addEventListener('show.bs.modal', function (event) {
+                const button = event.relatedTarget;
+                if (!button) return;
+
+                const reservationDate = button.getAttribute('data-reservation-date');
+                const reservationId = button.getAttribute('data-reservation-id');
+                const cancelUrl = button.getAttribute('data-cancel-url');
+                reservationText.textContent = reservationDate;
+                cancelConfirm.action = cancelUrl
 
 
-        });
+            });
+        }
 
         const editModal = document.getElementById('editReservationModal');
         const editForm = document.getElementById("editReservationForm");
         const partySizeInput = document.getElementById("id_party_size");
         const dateInput = document.getElementById("id_reservation_date");
         const timeInput = document.getElementById("id_reservation_time");
-        editModal.addEventListener('show.bs.modal', function (event) {
-            const button = event.relatedTarget;
-            const reservationDate = button.getAttribute('data-reservation-date');
-            const reservationTime = button.getAttribute('data-reservation-time');
-            const reservationPartySize = button.getAttribute('data-reservation-party-size');
-            const editUrl = button.getAttribute('data-edit-url');
-            dateInput.value = reservationDate;
-            timeInput.value = reservationTime;
-            partySizeInput.value = reservationPartySize;
-            editForm.action = editUrl
 
-        });
+        if(editModal && editForm && partySizeInput && dateInput && timeInput) {
+            editModal.addEventListener('show.bs.modal', function (event) {
+                const button = event.relatedTarget;
+                if (!button) return;
+
+
+                const reservationDate = button.getAttribute('data-reservation-date');
+                const reservationTime = button.getAttribute('data-reservation-time');
+                const reservationPartySize = button.getAttribute('data-reservation-party-size');
+                const editUrl = button.getAttribute('data-edit-url');
+                dateInput.value = reservationDate;
+                timeInput.value = reservationTime;
+                partySizeInput.value = reservationPartySize;
+                editForm.action = editUrl
+
+            });
+        }
     }
 });
