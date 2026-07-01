955b799ce8a4910bdde91b3ed23f80f66c437e35
alancoomeynts-hub <alancoomeynts@gmail.com>
2026-07-01 21:28:01 +0100

    update admin classes

--- bookings/admin.py
+++ bookings/admin.py
@@ -5,12 +5,13 @@ from .models import Table, Reservation
 
 @admin.register(Table)
 class TableAdmin(admin.ModelAdmin):
-    list_display = ('name','capacity','is_available',)
+    list_display = ('__str__',)
     list_filter = ('is_available','capacity',)
     search_fields = ('name',)
 
 @admin.register(Reservation)
 class ReservationAdmin(admin.ModelAdmin):
-    list_display = ('user','table','contact_name','contact_email','party_size','reservation_for')
+    list_display = ('__str__','table','party_size',)
     list_filter = ('reservation_for','table','user')
     search_fields = ('user__username','contact_name','contact_email')
+
--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Restaurant, SocialLink, Profile
+from .models import Restaurant, SocialLink, Profile, ContactRequest
+
 
 # Register your models here.
 @admin.register(Restaurant)
@@ -10,3 +11,10 @@ class RestaurantAdmin(SummernoteModelAdmin):
 admin.site.register(SocialLink)
 
 admin.site.register(Profile)
+
+@admin.register(ContactRequest)
+class ContactUsAdmin(SummernoteModelAdmin):
+    list_display = ('__str__',)
+    list_filter = ('request_type','reservation_for')
+    search_fields = ('request_type','name','email','phone')
+    summernote_fields = ('message',)
